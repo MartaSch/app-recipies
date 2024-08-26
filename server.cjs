6ca2d1a70dd0cc46c13c89edacf6fb2808893399
MartaSch <martaschab27@gmail.com>
2024-08-26 11:33:21 +0200

    update server.cjs

--- server.cjs
+++ server.cjs
@@ -3,8 +3,6 @@ const express = require('express');
 
 const app = express();
 
-const __dirname = path.dirname(__filename);
-
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
