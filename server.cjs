e37328ada77b2a10729e298c2c3c8f89648ed13d
MartaSch <martaschab27@gmail.com>
2024-08-26 11:57:39 +0200

    update server.cjs

--- server.cjs
+++ server.cjs
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
