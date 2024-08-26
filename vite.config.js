3af209b568399389ee9dd9486a15e140ab77e19a
MartaSch <martaschab27@gmail.com>
2024-08-26 12:47:26 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/yummy-app',
   plugins: [react()],
   build: {
     outDir: 'dist', // Make sure this matches your Express static file path
