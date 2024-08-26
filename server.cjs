bc609e49f30546170b16a37e0f6cb957b672d004
MartaSch <martaschab27@gmail.com>
2024-08-26 12:25:24 +0200

    update

--- server.cjs
+++ server.cjs
@@ -3,10 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yummy-app",
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Make sure this matches your Express static file path
+  },
 })
